f03cce5576c0381f71ad26795050c76845334a6c
seun <seun@example.com>
2019-11-22 01:36:27 +0900

    update from seun's desktop

--- scripts/practice/Explorer.py
+++ scripts/practice/Explorer.py
@@ -42,6 +42,8 @@ class Explorer(Laser_Scan, Pose_Scan):
             angle -= 360
         return angle
 
+
+
 class Exp(Laser_Scan, Pose_Scan):
     def __init__(self):
         Laser_Scan.__init__(self)
@@ -55,13 +57,37 @@ class Exp(Laser_Scan, Pose_Scan):
         if PosBag.mode == 'Explorer':
             drive = Drive_Method()
             if self.set_pose:
-                self.set_posture(drive) #자세잡기
+                self.set_posture(drive) #set head pose
 
     def set_starting(self): #init status at starting
         self. target_dir = 90.0
 
     def set_posture(self, drive):
+        turn_angle = self.rot_dir(self.angle, self.target_dir)
+        if self.target_dir - 45.0 < round(self.angle, 2) < self.target_dir + 45.0:
+            if turn_angle >= 0:  # should turn left
+                drive.angleTurnLeft()
+            else:  # should turn left
+                drive.angleTurnRight()
+            pass  # turn 1.0
+        elif self.target_dir - 29.0 < round(self.angle, 2) < self.target_dir + 29.0:
+            if turn_angle >= 0:  # should turn left
+                drive.smallTurnLeft()
+            else:  # should turn left
+                drive.smallTurnRight()
+            pass  # turn 0.4
+        elif self.target_dir - 12.0 < round(self.angle, 2) < self.target_dir + 12.0:
+            self.set_pose = False
+            return #finish set
+        else:
+            if turn_angle >= 0:  # should turn left
+                drive.turnLeft()
+            else:  # should turn left
+                drive.turnRight()
         drive.publish()
+
+    def expedition(self):   #drive linear
+        pass
 
     def get_direction(self, position):
         angle = 180.0 * math.atan2(self.position_y - position[1], self.position_x - position[0]) / math.pi
@@ -69,13 +95,10 @@ class Exp(Laser_Scan, Pose_Scan):
             angle += 360
         if angle >= 360:
             angle -= 360
-        return angle
+        return round(angle, 2)
 
     def rot_dir(self, pre, target):
         pre = (pre * math.pi / 180) - math.pi
         target = (target * math.pi / 180) - math.pi
-        result = math.atan2(math.sin(target - pre), math.cos(target - pre)) * (180 / math.pi)
-        if result >= 0:
-            pass #return  = turn right
-        else:
-            pass #return = turn left
+        angle = math.atan2(math.sin(target - pre), math.cos(target - pre)) * (180 / math.pi)
+        return angle(angle, 2)
